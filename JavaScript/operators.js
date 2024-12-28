3e673c967dda44dba3d0a16ffd2867bb6f1795ee
Dinesh Karthik Rajan D <dineshkarthikrajand.22cse@kongu.edu>
2024-12-28 11:29:41 +0530

    learning

---
+++ JavaScript/operators.js
@@ -0,0 +1,47 @@
+// 1.Assignment Operator
+
+var a = 10;
+var b = 10;
+
+// 2.Arithemtic Operator
+
+var sum = a + b;
+var diff = a - b;
+var mul = a * b;
+var divi = a / b;
+var modu = a % b;
+
+console.log(sum, diff, mul, divi, modu);
+
+// 3.Relational Operator
+s = 30
+let dif = "30"
+if(sum === diff)
+{
+    console.log("they are equal")
+}
+else{
+    console.log("not equal")
+}
+
+// 4.Logical Operator
+
+if((sum >= diff) && ((divi != modu) || (divi <= mul)))
+{
+    console.log("greater")
+}
+else{
+    console.log("not greater")
+}
+
+// 5.Bitwise Operator
+
+console.log(a << b)
+console.log(a >> b)
+console.log(a & b)
+
+// 6.Ternary Operator
+
+const age = 20
+let res = (age >= 20) ? "elegible" : "not elegible"
+console.log(res)
---
+++ JavaScript/secondaryDatatypes.js
@@ -0,0 +1,33 @@
+//Array Example
+// array = [10, 20, 30, [40.5, 50.6], "a", "KEC", true];
+// console.log(array);
+
+//Object Example Type 1
+
+// obj = {
+//   firstName: "Jai",
+//   lastName: "Kumar",
+//   age: 20,
+// };
+
+// console.log(obj);
+
+//Object Example Type2
+
+// objType2 = {};
+// objType2["movie"] = "Iron Man 2"
+// objType2["budget"] = "100cr"
+// objType2["boxOffice"] = "1000cr"
+// objType2["release"] = "Worldwide"
+// console.log(objType2);
+// console.log(objType2["boxOffice"])
+// console.log(objType2.budget)
+
+//Object Example Type3
+
+objType3 = new Object();
+objType3["phone"] = "andriod"
+objType3['store'] = "play store"
+objType3['game1'] = "PUBG"
+objType3['game2'] = "FreeFire"
+console.log(objType3);
